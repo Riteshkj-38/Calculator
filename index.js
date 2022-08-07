07505948403af78803189d97975d85b7cf8d927f
Riteshkj-38 <riteshkj.38@gmail.com>
2022-08-07 14:14:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var button = document.getElementsByClassName("Button");
+var display = document.getElementById("display");
+var operand1 = 0;
+var operand2 = null;
+var operand3 = null;
+
+for(var i=0;i<button.length;i++)
+{
+    button[i].addEventListener('click', fucntion(){
+     
+
+    });
+}
